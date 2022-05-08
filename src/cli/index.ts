12e1a92b7e8555a5084710e8462b2d61f14a08ea
Juan C Irizar <developer@jcirizar.com>
2022-05-08 03:11:45 -0400

    Cli output changes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command, CommanderError } from 'commander';
 import { parseIntValue } from '../utils';
 import { WaterJugSolver } from '../water-jug-solver';
 
 const chalk = require('chalk');
 
+// COLORS
+const yellow = chalk.yellow;
+const green = chalk.green;
+const error = chalk.bgRed.bold.black;
+const bBlue = chalk.bold.blue;
 
 const program = new Command();
 
@@ -18,50 +23,62 @@ const pad = (str: string, length: number, char = ' ') =>
   str.padStart((str.length + length) / 2, char).padEnd(length, char);
 
 program
-  .description('Water Jug Riddle')
+  .description('Water Jug Solver')
   .argument('<xGallon>', 'xGallon capacity', parseIntValue)
   .argument('<yGallon>', 'yGallon capacity', parseIntValue)
   .argument('<zGallon>', 'zGallon desired water amount', parseIntValue)
-  .action((xGallon, yGallon, zGallon, options) => {
-
-    const waterJugSolver = new WaterJugSolver(xGallon, yGallon, zGallon);
-    const [small, large] = [xGallon, yGallon].sort((a, b) => a - b);
-    const solution = waterJugSolver.solve();
-
-    const title = `===${pad('Water Jug Solver', 32)}===`;
-    const end = `===${pad('End', 32)}===`;
-
-    console.log(`${chalk.bgMagenta.bold.black(title)}
-    
-Solving for: ${chalk.bold.blue(zGallon)} gallons with:
-  - ${chalk.yellow('Small Bucket')}: ${chalk.bold.yellow(small)} gal
-  - ${chalk.green('Large Bucket')}: ${chalk.bold.green(large)} gal`);
+  .action(onAction);
 
 
-    if (typeof solution === 'string') {
-      console.log(`
+function onAction(xGallon: number, yGallon: number, zGallon: number) {
+  const waterJugSolver = new WaterJugSolver(xGallon, yGallon, zGallon);
+  const [small, large] = [xGallon, yGallon].sort((a, b) => a - b);
+  const solution = waterJugSolver.solve();
+
+  console.log(`Solving for: ${bBlue(zGallon)} gallons with:
+  - ${yellow('Small Bucket')}: ${yellow.bold(small)} gal
+  - ${green('Large Bucket')}: ${green.bold(large)} gal`);
+
+
+  if (typeof solution === 'string') {
+    console.log(`
 ${chalk.cyan('Result:')}`);
-      console.log(`  - ${chalk.bgGrey.red(solution)}`);
-    } else {
-      console.log(`
-${chalk.cyan('Steps:')}`);
-      solution.forEach((jugState, index) => {
-        if(index !== solution.length - 1) {
-          console.log(`  ${index + 1}- ${chalk.cyan(jugState.action.padEnd(15))}=> ${chalk.yellow('Small')}: ${chalk.yellow.bold(jugState.jugs[0].value)} ${chalk.green('Large')}: ${chalk.green.bold(jugState.jugs[1].value)}`);
-        } else {
-          console.log(`  ${index + 1}- ${chalk.cyan(jugState.action.padEnd(15))}=> ${chalk.yellow('Small')}: ${chalk.yellow.bold(jugState.jugs[0].value)} ${chalk.green('Large')}: ${chalk.green.bold(jugState.jugs[1].value)}`);
-        }
-      });
-    }
+    console.log(`  - ${chalk.bgGrey.red(solution)}`);
+  } else {
+    console.log(`
+${chalk.cyan('Steps')}:`);
+    solution.forEach((jugState, index) => {
+      const smallValue = jugState.jugs[0].value;
+      const largeValue = jugState.jugs[1].value;
 
-    console.log(`\n${chalk.bgMagenta.bold.black(end)}`);
-  });
-
-try {
-  program.parse();
-} catch (e) {
-  if (e instanceof Error) {
-    console.log(`error: ${e.message}`);
+      if (index !== solution.length - 1) {
+        console.log(`  ${index + 1}- ${chalk.cyan(jugState.action.padEnd(15))}=> ${yellow('Small')}: ${yellow(smallValue)} ${green('Large')}: ${green(largeValue)}`);
+      } else {
+        const highlight = chalk.bgGray.bold.blue;
+        const smallColor = zGallon === smallValue ? highlight : (zGallon === smallValue + largeValue ? highlight : yellow);
+        const largeColor = zGallon === largeValue ? highlight : (zGallon === smallValue + largeValue ? highlight : green);
+        console.log(`  ${index + 1}- ${chalk.cyan(jugState.action.padEnd(15))}=> ${yellow('Small')}: ${smallColor(smallValue)} ${green('Large')}: ${largeColor(largeValue)}`);
+      }
+    });
   }
+
 }
 
+
+const title = `===${pad('Water Jug Solver', 32)}===\n`;
+const end = `===${pad('End', 32)}===`;
+console.log(`${chalk.bgMagenta.bold.black(title)}`);
+try {
+  program.exitOverride().configureOutput({
+    outputError: () => {
+    }
+  });
+  program.parse();
+} catch (e) {
+  if (e instanceof CommanderError) {
+    console.log(error(`${e.message}`));
+  } else {
+    console.log(error(`error: ${(<Error>e).message}`));
+  }
+}
+console.log(`\n${chalk.bgMagenta.bold.black(end)}`);
